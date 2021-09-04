5ec56c0e9c9a336c7b13874a76b9587b5568fc15
Daniel Calvin <arockne2003@yahoo.com>
2021-09-04 16:32:12 -0700

    Implement a conveyor of sushi

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,14 +1,7 @@
 import React, { useState } from "react";
 
 function MoreButton({ onMoreSushi }) {
-  const [fourMore, setFourMore] = useState(4)
-
-  function handleAdditionalSushi() {
-    setFourMore(fourMore => fourMore + 4);
-    onMoreSushi(fourMore);
-  }
-
-  return <button onClick={handleAdditionalSushi}>More sushi!</button>;
+  return <button onClick={onMoreSushi}>More sushi!</button>;
 }
 
 export default MoreButton;
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -4,12 +4,18 @@ import Sushi from './Sushi'
 
 function SushiContainer({ sushi, onPlates }) {
   const [fourSushi, setFourSushi] = useState([...sushi])
+  const [fourMore, setFourMore] = useState(4)
 
   useEffect(() => {
     setFourSushi(sushi.slice(0,4))
   }, [sushi])
 
-  function handleMoreSushi(fourMore) {
+  function handleMoreSushi() {
+    if ((fourMore + 4) < sushi.length) {
+      setFourMore(fourMore => fourMore + 4)
+    } else {
+      setFourMore(0)
+    }
     setFourSushi(sushi.slice(fourMore, fourMore + 4))
   }
 
